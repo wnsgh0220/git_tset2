8b89eed3ed11fccdd25e6d0d6cc674f64c78657b
Park, Junho <wnsgh0220@gmail.com>
2026-09-21 10:38:10 +0900

    main 메소드 수정

--- src/git_test2/Main.java
+++ src/git_test2/Main.java
@@ -1,7 +1,7 @@
 package git_test2;
 public class Main {
-	
-	public static void main(String[] args) {
-		new Hello().say();
+ public static void main(String[] args) {
+ Hello h = new Hello();
+ h.say();
  }
 }
